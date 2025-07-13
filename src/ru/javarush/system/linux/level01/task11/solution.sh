4efd3e2fe1546b364eeebd8a28fe8992bb78783c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-13 20:00:27 +0300

    add solution level 01 task 11

---
+++ src/ru/javarush/system/linux/level01/task11/solution.sh
@@ -0,0 +1,10 @@
+# Создаем папку project с вложенными папками src и docs
+
+
+# Создаем файл readme.txt в папке docs
+
+
+# Копируем файл readme.txt из папки docs в папку src
+
+
+# Перемещаем файл readme.txt из папки src в корневую папку project

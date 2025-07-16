4be6947e2502eb61e1ef9873b9d8c28937c69550
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-16 11:17:45 +0300

    add solution level 05 task 11

---
+++ src/ru/javarush/system/linux/level05/task11/solution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Переводим SELinux в permissive-режим
+
+
+# Проверяем, что SELinux находится в permissive-режиме
+
+
+# Переводим SELinux обратно в enforcing-режим
+
+
+# Проверяем, что SELinux находится в enforcing-режиме

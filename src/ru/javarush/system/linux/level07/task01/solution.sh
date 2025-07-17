b5a8369e80d6afd2c647a630f7b947bd7d56ade5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-17 15:59:43 +0300

    add solution level 07 task 01

---
+++ src/ru/javarush/system/linux/level07/task01/solution.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Установите веб-сервер Nginx
+sudo apt update
+sudo apt install -y nginx
+
+# Запустите Nginx и проверьте его статус
+???
+
+# Откройте браузер и проверьте http://localhost
+echo "Перейдите в браузере по адресу http://localhost, чтобы увидеть стандартную страницу Nginx."

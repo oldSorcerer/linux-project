4cb40a85c8923a759c0dc94ddf3ba4e8ab887df6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-17 16:00:45 +0300

    add solution level 07 task 13

---
+++ src/ru/javarush/system/linux/level07/task13/solution.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1. Обновите систему
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# 2. Установите Certbot и соответствующий плагин для веб-сервера
+
+# Для Nginx
+???
+
+# Для Apache
+???
+
+# 3. Убедитесь, что Certbot установлен
+???

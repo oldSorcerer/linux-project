cb38d0424d7b034966dd4f764c28464a2e0f05c0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-07-17 15:59:49 +0300

    add solution level 07 task 02

---
+++ src/ru/javarush/system/linux/level07/task02/solution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Установите веб-сервер Apache
+sudo apt update
+sudo apt install -y apache2
+
+# Запустите Apache и проверьте его статус
+???
+
+
+# Откройте браузер и проверьте http://localhost
+echo "Перейдите в браузере по адресу http://localhost, чтобы увидеть стандартную страницу Apache."
